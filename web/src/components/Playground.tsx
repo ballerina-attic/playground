499901198fcc4a7ecaf49b82686a9af7c7d17616
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-10-15 16:47:18 +0530

    Configure less for styling

--- web/src/components/Playground.tsx
+++ web/src/components/Playground.tsx
@@ -1,3 +1,4 @@
 import * as React from "react";
+import './Playground.less'
 
 export const Playground = () => <h1>Playground</h1>
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -25,7 +25,21 @@ module.exports = {
                 enforce: "pre",
                 test: /\.js$/,
                 loader: "source-map-loader"
-            }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                  {
+                    loader: 'style-loader', // creates style nodes from JS strings
+                  },
+                  {
+                    loader: 'css-loader', // translates CSS into CommonJS
+                  },
+                  {
+                    loader: 'less-loader', // compiles Less to CSS
+                  },
+                ],
+            },
         ]
     },
 
