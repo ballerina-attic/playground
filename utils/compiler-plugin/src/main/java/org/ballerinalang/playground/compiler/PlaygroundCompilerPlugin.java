aff69adeb22453f8c654fdf272c2e8cb1711a1f4
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-10-23 10:25:01 +0530

    Fix compiler plugin not working issue

--- utils/compiler-plugin/src/main/java/org/ballerinalang/playground/compiler/PlaygroundCompilerPlugin.java
+++ utils/compiler-plugin/src/main/java/org/ballerinalang/playground/compiler/PlaygroundCompilerPlugin.java
@@ -1,25 +1,34 @@
 package org.ballerinalang.playground.compiler;
 
-
 import org.ballerinalang.compiler.plugins.AbstractCompilerPlugin;
-import org.ballerinalang.model.tree.AnnotationAttachmentNode;
-import org.ballerinalang.model.tree.ServiceNode;
+import org.ballerinalang.model.tree.NodeKind;
+import org.ballerinalang.model.tree.PackageNode;
+import org.ballerinalang.util.diagnostic.Diagnostic;
 import org.ballerinalang.util.diagnostic.DiagnosticLog;
 
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlaygroundCompilerPlugin extends AbstractCompilerPlugin {
 
+    private DiagnosticLog diagnosticLog;
 
     @Override
     public void init(DiagnosticLog diagnosticLog) {
-
+        this.diagnosticLog = diagnosticLog;
     }
 
     @Override
-    public void process(ServiceNode serviceNode, List<AnnotationAttachmentNode> annotations) {
-        if (serviceNode != null) {
-            throw new RuntimeException("Running services in playground is not allowed.");
-        }
+    public void process(PackageNode packageNode) {
+        packageNode.getCompilationUnits()
+                .forEach(cu -> {
+                    cu.getTopLevelNodes()
+                            .stream()
+                            .filter(topLevelNode -> topLevelNode.getKind().equals(NodeKind.SERVICE))
+                            .collect(Collectors.toList())
+                            .forEach(serviceNode -> {
+                                diagnosticLog.logDiagnostic(Diagnostic.Kind.ERROR, serviceNode.getPosition(),
+                                        "Running Ballerina services is not allowed in playground.");
+                            });
+                });
     }
 }
