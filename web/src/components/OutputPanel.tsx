e39198769fa97e17507ceb5e13f4a91348779831
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-11-05 18:02:21 +0530

    Change appearance of msgs depending on type

--- web/src/components/OutputPanel.tsx
+++ web/src/components/OutputPanel.tsx
@@ -8,8 +8,24 @@ export function OutputPanel() {
             <div className="output-panel w3-container">
                 {
                     responses.map(({ type, data }) => (
-                        <div>
-                            {type}: {data}
+                        <div className={type.toLowerCase()}>
+                           {data.split(/\n/g).map((val) => {
+                                const tabbedPieces = val.split(/\t/g);
+                                return (
+                                    <div>
+                                        {
+                                            tabbedPieces.length === 1 && <span>{val}</span>
+                                        }
+                                        {
+                                            tabbedPieces.length > 1 &&
+                                                val.split(/\t/g)
+                                                    .map((tabbedSegment) =>
+                                                        <span>&nbsp;&nbsp;&nbsp;&nbsp;{tabbedSegment}</span>,
+                                                    )
+                                        }
+                                    <br/></div>
+                                );
+                            })}
                         </div>
                     ))
                 }
