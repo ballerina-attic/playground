d809534126fb1bae724f1ab60b2b65b16fc99602
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-11-05 19:50:39 +0530

    Disable diagram & share btns

--- web/src/components/ControlPanel.tsx
+++ web/src/components/ControlPanel.tsx
@@ -6,8 +6,8 @@ export function ControlPanel() {
     return <div className="control-panel w3-top w3-teal">
                 <div className="w3-bar">
                     <RunButton />
-                    <button className="w3-button w3-white">Diagram</button>
-                    <button className="w3-button w3-white w3-right">Share</button>
+                    <button className="w3-button w3-white w3-disabled">Diagram</button>
+                    <button className="w3-button w3-white w3-right w3-disabled">Share</button>
                 </div>
             </div>;
 }
