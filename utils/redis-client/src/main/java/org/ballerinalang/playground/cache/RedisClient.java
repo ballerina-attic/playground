158e34605cc93a7d65d720a217b7a5a7967c5ffc
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-11-17 08:24:44 +0530

    Add a java lib wrapping jedis for interop

---
+++ utils/redis-client/src/main/java/org/ballerinalang/playground/cache/RedisClient.java
@@ -0,0 +1,54 @@
+package org.ballerinalang.playground.cache;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Redis Client
+ */
+public class RedisClient {
+
+    private static JedisPool masterPool;
+    private static JedisPool slavePool;
+
+    private static int port = 6379;
+
+    private static RedisClient instance;
+
+    private RedisClient() {
+        String writeHost = System.getenv("REDIS_WRITE_HOST");
+        String readHost = System.getenv("REDIS_READ_HOST");
+        if (writeHost == null|| readHost == null) {
+            throw new IllegalArgumentException("Missing required environment variables for redis client." );
+        }
+        masterPool = new JedisPool(new JedisPoolConfig(),
+                writeHost,
+                port);
+        slavePool = new JedisPool(new JedisPoolConfig(),
+                readHost,
+                port);
+    }
+
+    public static RedisClient getInstance () {
+        if (instance == null) {
+            instance = new RedisClient();
+        }
+        return instance;
+    }
+
+    public Jedis getWriteClient() {
+        return slavePool.getResource();
+    }
+
+    public Jedis getReadClient() {
+        return masterPool.getResource();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        masterPool.close();
+        slavePool.close();
+    }
+}
