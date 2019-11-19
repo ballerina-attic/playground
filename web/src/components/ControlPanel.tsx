9ec2a393506e4653070ec5e177d1e2eee6aca3f9
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-11-19 13:46:29 +0530

    Display msg & copy to clipboard once shared

--- web/src/components/ControlPanel.tsx
+++ web/src/components/ControlPanel.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import "./ControlPanel.less";
+import { PlaygroundContext } from "./Playground";
 import { RunButton } from "./RunButton";
 import { ShareButton } from "./ShareButton";
 
 export function ControlPanel() {
-    return <div className="control-panel w3-top w3-teal">
-                <div className="w3-bar">
-                    <img className="logo" src="images/ballerina-logo-play.svg" />
-                    <RunButton />
-                    <ShareButton />
-                </div>
-            </div>;
+    return <PlaygroundContext.Consumer>
+    { ({ displayCopiedToCB }) => (
+        <div className="control-panel w3-top w3-teal">
+            <div className="w3-bar">
+                <img className="logo" src="images/ballerina-logo-play.svg" />
+                <RunButton />
+                <ShareButton />
+                {displayCopiedToCB &&
+                    <span
+                        className="w3-text w3-small w3-animate-opacity">
+                            copied to clipboard.
+                    </span>
+                }
+            </div>
+        </div>)}
+    </PlaygroundContext.Consumer>;
 }
